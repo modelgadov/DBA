b6a1709c60c67e5cc5f5177de152fcba0a68f4a0
modelgadov <mdelgadov@gmd.com.pe>
2017-01-12 18:08:11 -0500

    Update sql_server_querys_informacion2.sql

--- sql_server_querys_informacion2.sql
+++ sql_server_querys_informacion2.sql
@@ -35,3 +35,13 @@ SELECT name AS object_name
 FROM sys.objects
 WHERE modify_date > GETDATE() - 10
 ORDER BY modify_date;
+
+-----Status del database mirroring
+SELECT
+DB_NAME(database_id) As DatabaseName,
+CASE WHEN mirroring_guid IS NOT NULL THEN 'Mirroring is On' ELSE 'No mirror configured' END AS IsMirrorOn,
+mirroring_state_desc,
+CASE WHEN mirroring_safety_level=1 THEN 'High Performance' WHEN mirroring_safety_level=2 THEN 'High Safety' ELSE NULL END AS MirrorSafety,
+mirroring_role_desc,
+mirroring_partner_instance AS MirrorServer
+FROM sys.database_mirroring
